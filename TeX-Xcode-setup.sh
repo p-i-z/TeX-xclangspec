4eedec413650913dccaa013796137ed4cfd959e8
Patrick Iglesias-Zemmour <71599886+p-i-z@users.noreply.github.com>
2025-07-26 19:36:16 +0200

    Update TeX-Xcode-setup.sh
    .sh script that put files in place

--- TeX-Xcode-setup.sh
+++ TeX-Xcode-setup.sh
@@ -1,44 +1,70 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Path were this script is located
-#
-SCRIPT_PATH="$(dirname "$BASH_SOURCE")"
+# TeX-Xcode-setup.sh
+# A script to install the TeX syntax highlighting plugin for Xcode.
+# It copies the necessary files to their system locations.
 
-# Various directories for the plugin
-#
-plugins_dir=~/Library/Developer/Xcode/Plug-ins/
+# Exit immediately if any command fails
+set -e
 
-spec_dir=/Applications/Xcode.app/Contents/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageSpecifications
+echo "Starting TeX for Xcode installation..."
 
-metdat_dir=/Applications/Xcode.app/Contents/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageMetadata
+# --- Define Source and Destination Paths ---
+# Assume the script is run from the root of the project folder.
+SOURCE_DIR=$(pwd)
 
-FontAndColorThemes_dir=~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+# Destination directories
+PLUGINS_DIR=~/Library/Developer/Xcode/Plug-ins
+SPECS_DIR="/Applications/Xcode.app/Contents/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageSpecifications"
+METADATA_DIR="/Applications/Xcode.app/Contents/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageMetadata"
+THEMES_DIR=~/Library/Developer/Xcode/UserData/FontAndColorThemes
 
-# Create Plug-ins directory if it doesn't exist
-if [ ! -d "$plugins_dir" ]; then
-    mkdir $plugins_dir
+# --- Verify Source Files Exist ---
+if ! [ -d "$SOURCE_DIR/TeX.ideplugin" ] || ! [ -f "$SOURCE_DIR/TeX.xclangspec" ]; then
+    echo "Error: Source files not found."
+    echo "Please make sure you are running this script from inside the 'TeX-xclangspec GitHub' directory."
+    exit 1
 fi
 
-# Create Specifications directory if it doesn't exist
-if [ ! -d "$spec_dir" ]; then
-    mkdir $spec_dir
-fi
-    
-# Create LangageMetadat directory if it doesn't exist
-if [ ! -d "$metdat_dir" ]; then
-    mkdir $metdat_dir
-fi
+# --- Create Directories and Copy Files ---
 
-# Create FontAndColorThemes directory if it doesn't exist
-if [ ! -d "$FontAndColorThemes_dir" ]; then
-    mkdir $FontAndColorThemes_dir
-fi
+echo "Creating directories if they do not exist..."
+mkdir -p "$PLUGINS_DIR"
+# Note: System directories require sudo to create, but they should already exist.
+# We will rely on sudo for the copy command if needed.
+mkdir -p "$THEMES_DIR"
+
+echo "Installing files..."
+
+# User-level files
+echo "Installing TeX.ideplugin..."
+cp -R "$SOURCE_DIR/TeX.ideplugin" "$PLUGINS_DIR/"
+
+echo "Installing Basic TeX.xccolortheme..."
+cp "$SOURCE_DIR/Basic TeX.xccolortheme" "$THEMES_DIR/"
+
+# System-level files
+# These may require a password if System Integrity Protection is restrictive.
+echo ""
+echo "You may be asked for your administrator password to install system-level components."
+
+echo "Installing TeX.xclangspec..."
+sudo cp "$SOURCE_DIR/TeX.xclangspec" "$SPECS_DIR/"
+
+echo "Installing Xcode.SourceCodeLanguage.TeX.plist..."
+sudo cp "$SOURCE_DIR/Xcode.SourceCodeLanguage.TeX.plist" "$METADATA_DIR/"
 
 
-cp -r $SCRIPT_PATH/TeX.ideplugin $plugins_dir
+# --- Final Instructions ---
+echo ""
+echo "--------------------------------------------------"
+echo "Installation Complete!"
+echo ""
+echo "IMPORTANT NEXT STEPS:"
+echo "1. Quit and Restart Xcode completely."
+echo "2. When Xcode launches, click 'Load Bundle' when prompted."
+echo "3. Go to Xcode > Settings > Themes and select 'Basic TeX'."
+echo "--------------------------------------------------"
+echo ""
 
-cp $SCRIPT_PATH/TeX.xclangspec $spec_dir
-
-cp $SCRIPT_PATH/Xcode.SourceCodeLanguage.TeX.plist $metdat_dir
-
-cp $SCRIPT_PATH/"Basic TeX.xccolortheme" $FontAndColorThemes_dir
+exit 0
